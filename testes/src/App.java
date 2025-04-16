085cdcc86ae9e12bf39b06617b78409c8f2bb1d7
Kaiky <kakamg6@gmail.com>
2025-04-16 18:47:13 -0300

    testes

--- testes/src/App.java
+++ testes/src/App.java
@@ -2,7 +2,7 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        String name = "fudido";
+        String name = "fudidos";
         System.out.println(name);
     }
 }
